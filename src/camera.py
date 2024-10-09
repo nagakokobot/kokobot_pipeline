31088c9d0032c0a3143bb1a52751280c9bfd08b5
nagakokobot <naga.thesis@gmail.com>
2024-10-09 14:58:51 +0200

    - added camera.py to save the rgb and depth images - next:  - object detection and get the tool list and locations (possible transformations need to be tracked)

---
+++ src/camera.py
@@ -0,0 +1,62 @@
+import pyzed.sl as sl
+import cv2
+
+
+class get_images:
+    #TODO: make the class to incorporate stereo depth.
+   
+
+    def __init__(self,initparameters, runtimeparameters, save_path,show_workspace:bool = False):
+        
+        self.initpara = initparameters
+        self.runtimepara = runtimeparameters
+        self.s_path = save_path
+        self.show_workspace = show_workspace
+
+        zed = self.open_camera(self.initpara)
+        s_number = zed.get_camera_information().serial_number
+        print('Camera object created for the camera serial number:', s_number)
+        rgb, depth = self.get_mats(zed,self.runtimepara)
+        if self.show_workspace:
+            named_window = f'workspace_{s_number}'
+            cvImage = rgb.get_data()
+            cv2.imshow(named_window, cvImage)
+            print('press any to exit and continue execution')
+            cv2.waitKey(4000)
+        self.save_rgbd(rgb, depth, self.s_path)
+        
+        cv2.destroyAllWindows()
+        zed.close()
+            
+
+    def open_camera(self, initparams):
+        cam_object = sl.Camera()
+        err = cam_object.open(initparams)
+        if err != sl.ERROR_CODE.SUCCESS:
+            print(repr(err))
+            cam_object.close()
+            exit()
+        
+        return cam_object
+    
+    def get_mats(self, cam_object,runtimeparams):
+        rgb_mat = sl.Mat()
+        depth_mat = sl.Mat()
+
+        new_frame_err = cam_object.grab(runtimeparams)
+        if new_frame_err == sl.ERROR_CODE.SUCCESS:
+            cam_object.retrieve_image(rgb_mat, sl.VIEW.LEFT)
+            cam_object.retrieve_measure(depth_mat, sl.MEASURE.DEPTH)
+
+        return rgb_mat, depth_mat
+
+    def save_rgbd(self, rgb_mat, depth_mat, save_path):
+        img_path = save_path + '/rgb_image.png'
+        d_path = save_path+'/true_depth.tiff'
+
+        cv2.imwrite(img_path, rgb_mat.get_data())
+        print('saved rgb image')
+        cv2.imwrite(d_path, depth_mat.get_data())
+        print('saved true_depth.tiff')
+  
+        pass
--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 from camera_parameters import get_camera_serial_number, get_init_camera_paramaters, get_runtime_camera_parameters
 from helpers import create_folder
+from camera import get_images
 
 
 def parse_args():
@@ -14,7 +15,7 @@ def parse_args():
     #parser.add_argument('--name', type=str, required=True, help="Name parameter (mandatory).")
     #for init_parameters
     parser.add_argument('--camera-resolution', type=str, default= 'HD720', choices=['HD720', 'HD1080'], help="Name parameter (mandatory).")
-    parser.add_argument('--fps', type=int, choices=[15,30,60], help = 'Frames per second required (depends on the resolution)') #TODO: fix the problem with argparse
+    #parser.add_argument('--fps', type=int, choices=[15,30,60], help = 'Frames per second required (depends on the resolution)') #TODO: fix the problem with argparse
 
     #for runtime_parameters
     #parser.add_argument('--measure3D-reference-frame', type=str, default= 'camera', choices=['world', 'camera'], help = 'reference frame for next camera frame')
@@ -32,7 +33,7 @@ if __name__ == '__main__':
 
     #for testing:
     sys.argv = ['main.py', '--camera-resolution', 'HD1080']
-    sys.argv = ['main.py', '--fps', 30]
+    #sys.argv = ['main.py', '--fps', 30]
     #sys.argv = ['main.py', '--measure3D-reference-frame', 'world']
     #sys.argv = ['main.py', '--confidence-threshold', 50]
     #sys.argv = ['main.py', '--texture-confidence-threshold', 50]
@@ -58,3 +59,4 @@ if __name__ == '__main__':
 
         init_params = get_init_camera_paramaters(args = args, serial_number= serial_number, save_path = s_path)
         runtime_params = get_runtime_camera_parameters(args = args, save_path=s_path)
+        get_images(initparameters= init_params, runtimeparameters=runtime_params, save_path= s_path, show_workspace= True)
